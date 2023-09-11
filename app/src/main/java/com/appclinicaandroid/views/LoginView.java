6e98c863e6d5996fea9d68f94c82c8fb87e67d4e
pumaillekevin <pumaillekevin@gmail.com>
2023-09-10 19:16:06 -0500

    Primer commit

---
+++ app/src/main/java/com/appclinicaandroid/views/LoginView.java
@@ -0,0 +1,16 @@
+package com.appclinicaandroid.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.appclinicaandroid.R;
+
+public class LoginView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
