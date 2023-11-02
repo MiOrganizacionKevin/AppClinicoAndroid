b2c2b6e002e2347040b02c5f7e09fe3aa595cb71
dd0211 <dd6161165@gmail.com>
2023-11-02 16:01:31 -0500

    Update Vista Registro de Usuario

--- app/src/main/java/com/example/myapplication/view/Register_Activity.java
+++ app/src/main/java/com/example/myapplication/view/Register_Activity.java
@@ -16,8 +16,8 @@ import java.util.Calendar;
 
 public class Register_Activity extends AppCompatActivity {
 
-    private EditText dniEditText, nombreEditText, apellidoPaternoEditText, apellidoMaternoEditText,
-            sexoEditText, emailEditText, telefonoEditText, contrasenaEditText;
+    private EditText dniEditText, nombreEditText, apellidoEditText,
+            emailEditText, contrasenaEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,62 +25,29 @@ public class Register_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         // Inicializa los elementos de la interfaz
+
         dniEditText = findViewById(R.id.dniEditText);
         nombreEditText = findViewById(R.id.nombreEditText);
-        apellidoPaternoEditText = findViewById(R.id.apellidoPaternoEditText);
-        apellidoMaternoEditText = findViewById(R.id.apellidoMaternoEditText);
-        sexoEditText = findViewById(R.id.sexoEditText);
+        apellidoEditText = findViewById(R.id.apellidoEditText);
         emailEditText = findViewById(R.id.emailEditText);
-        telefonoEditText = findViewById(R.id.telefonoEditText);
         contrasenaEditText = findViewById(R.id.contrasenaEditText);
 
-        EditText fechaNacimientoEditText = findViewById(R.id.fechaNacimientoEditText);
         Button registrarseButton = findViewById(R.id.registrarseButton);
 
-        fechaNacimientoEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDatePickerDialog();
-            }
-
-            private void showDatePickerDialog() {
-                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
-                        fechaNacimientoEditText.setText(selectedDate);
-                    }
-                };
-
-                Calendar calendar = Calendar.getInstance();
-                int year = calendar.get(Calendar.YEAR);
-                int month = calendar.get(Calendar.MONTH);
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog datePickerDialog = new DatePickerDialog(Register_Activity.this, dateSetListener, year, month, day);
-                datePickerDialog.show();
-            }
-
-        });
         registrarseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String dni = dniEditText.getText().toString();
                 String nombre = nombreEditText.getText().toString();
-                String apellidoPaterno = apellidoPaternoEditText.getText().toString();
-                String apellidoMaterno = apellidoMaternoEditText.getText().toString();
-                String sexo = sexoEditText.getText().toString();
-                String fechaNacimiento = fechaNacimientoEditText.getText().toString();
+                String apellido = apellidoEditText.getText().toString();
                 String email = emailEditText.getText().toString();
-                String telefono = telefonoEditText.getText().toString();
                 String contrasena = contrasenaEditText.getText().toString();
 
-               /* Intent intent = new Intent(Register_Activity.this, Login_Activity.class);
+                /*Intent intent = new Intent(Register_Activity.this, Login_Activity.class);
                 startActivity(intent);*/
+
             }
         });
-
-
 
     }
 }
