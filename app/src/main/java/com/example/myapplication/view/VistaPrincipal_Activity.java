c62624c71494c91dc8f191dad0a05be18154cf7a
dd0211 <dd6161165@gmail.com>
2023-11-02 15:49:05 -0500

    Update Vista Principal

--- app/src/main/java/com/example/myapplication/view/VistaPrincipal_Activity.java
+++ app/src/main/java/com/example/myapplication/view/VistaPrincipal_Activity.java
@@ -8,25 +8,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.myapplication.R;
 
 public class VistaPrincipal_Activity extends AppCompatActivity {
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_principal);
 
-        Button misCitasButton = findViewById(R.id.misCitasButton);
-        Button registrarCitaButton = findViewById(R.id.registrarCitaButton);
+        ImageButton misCitasButton = findViewById(R.id.misCitasButton);
+        ImageButton registrarCitaButton = findViewById(R.id.registrarCitasButton);
+        ImageButton misCitas2Button = findViewById(R.id.misCitas2Button);
 
         misCitasButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*Intent intent = new Intent(MainActivity.this, Citas_Activity.class);
+                /*Intent intent = new Intent(VistaPrincipal_Activity.this, Citas_Activity.class);
+                startActivity(intent);*/
+            }
+        });
+
+        misCitas2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent intent = new Intent(VistaPrincipal_Activity.this, Citas_Activity.class);
                 startActivity(intent);*/
             }
         });
@@ -34,11 +42,10 @@ public class VistaPrincipal_Activity extends AppCompatActivity {
         registrarCitaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*Intent intent = new Intent(MainActivity.this, Register_Citas_Activity.class);
+                /*Intent intent = new Intent(VistaPrincipal_Activity.this, Register_Citas_Activity.class);
                 startActivity(intent);*/
             }
         });
-
 
     }
 }
