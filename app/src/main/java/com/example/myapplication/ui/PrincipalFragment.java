99bdc58ba58c8f309c4f145ff8ce4e0f3bfe3475
dd0211 <dd6161165@gmail.com>
2023-11-12 00:05:49 -0500

    Avance Implementacion Navigation Drawer

--- app/src/main/java/com/example/myapplication/ui/PrincipalFragment.java
+++ app/src/main/java/com/example/myapplication/ui/PrincipalFragment.java
@@ -1,37 +1,69 @@
 package com.example.myapplication.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toolbar;
+import android.widget.ImageButton;
 
 import com.example.myapplication.R;
-import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class PrincipalFragment extends Fragment {
 
-    public PrincipalFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    //Metodo para mostrar contenido del fragmento
-    @Override
+    /*@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_principal, container, false);
-
         // Inflate the layout for this fragment
-        return view;
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }*/
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_principal, container, false);
+
+        ImageButton misCitasButton = rootView.findViewById(R.id.misCitasButton);
+        ImageButton registrarCitaButton = rootView.findViewById(R.id.registrarCitasButton);
+        ImageButton misCitas2Button = rootView.findViewById(R.id.misCitas2Button);
+
+        misCitasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(getActivity(), Citas_Activity.class);
+                //startActivity(intent);
+            }
+        });
+
+        misCitas2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(getActivity(), Citas_Activity.class);
+                //startActivity(intent);
+            }
+        });
+
+        registrarCitaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abre el fragmento de registro de citas (RegisterCitasFragment)
+                /*Fragment registerCitasFragment = new RegistrarCitaFragment();
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment_content_main, registerCitasFragment); // Reemplaza el contenido actual con el fragmento de registro de citas
+                transaction.addToBackStack(null); // Opcional: agrega la transacción a la pila de retroceso
+                transaction.commit();*/
+            }
+
+        });
+        //registrarCitaButton.setOnClickListener(v -> selectFragment(new RegistrarCitaFragment()));
+//nav_host_fragment_content_main
+        return rootView;
+    }
+
+    void selectFragment(Fragment f){
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.drawer_layout,f).commit();
     }
 }
---
+++ app/src/main/java/com/example/myapplication/view/Register_Cita_Activity.java
@@ -0,0 +1,58 @@
+package com.example.myapplication.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+
+public class Register_Cita_Activity extends AppCompatActivity {
+
+    private Spinner especialidadSpinner;
+    private Button buscarButton;
+
+    String[] items = {"Medicina", "Pediatría", "Neurología","Cardiología"};
+
+    AutoCompleteTextView autoCompleteTextView;
+
+    ArrayAdapter<String> adapterItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_cita);
+
+        autoCompleteTextView = findViewById(R.id.especialidadTextView);
+
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_item,items);
+
+        autoCompleteTextView.setAdapter(adapterItems);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), "Especialidad: "+item,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buscarButton = findViewById(R.id.buscarButton);
+
+        buscarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acciones a realizar cuando se hace clic en el botón de búsqueda
+                //String especialidadSeleccionada = especialidadSpinner.getSelectedItem().toString();
+            }
+        });
+
+
+    }
+}
